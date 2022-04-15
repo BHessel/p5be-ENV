457d952c74764a40675428f23d114cf1377a929d
Ben Hessel <benhessel@Bens-MacBook-Air.local>
2022-04-15 12:56:51 -0400

    initializers added

---
+++ config/initializers/cors.rb
@@ -0,0 +1,15 @@
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+    allow do
+        origin "http://localhost:3000"
+        resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    end
+    
+    # allow do
+    #     # this is where production goes
+    #     # the origin here should be whatever I'm deployed to, e.g. https://blahblah.herokuapp.com
+    #     origin "http://localhost:3000"
+    #     resource "*", headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true
+    # end
+
+end
+
