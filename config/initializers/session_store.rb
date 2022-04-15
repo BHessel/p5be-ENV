c73af28fe27812e65b31308d750f4a32b887fef5
Ben Hessel <benhessel@Bens-MacBook-Air.local>
2022-04-15 13:46:49 -0400

    backend done

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,1 +1,9 @@
-Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "http://localhost:3001/"
+if Rails.env == "production"
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app", domain: "http://localhost:3001/"
+else
+    Rails.application.config.session_store :cookie_store, key: "_authentication_app"
+end
+
+# if there are issues reference this video https://www.youtube.com/watch?v=1JRdaemJNh4&list=PLgYiyoyNPrv_yNp5Pzsx0A3gQ8-tfg66j&index=4&ab_channel=edutechional
+
+
